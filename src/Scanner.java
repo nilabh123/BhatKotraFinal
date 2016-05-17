de8a5355aa6cc56bd03dba26b41d51c0b07e2536
unknown <nbhat593@FHS-LAB127-L25.fhs.win.fuhsd.org>
2016-05-17 07:37:10 -0700

    Updated

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,17 +1,18 @@
+import java.util.ArrayList;
+
 import processing.core.PImage;
 
 public class Scanner {
-	PImage student;
-	PImage key;
 	
-	public Scanner(PImage key, PImage student){
+	int[] student;
+	int[] key;
+	int scantron_length;
+	
+	public Scanner(int[] key, int[] student){
 		this.key = key;
 		this.student = student;
 	}
-	
-	public int[] convertImage(PImage image){
-		
-	}
+
 	
 	public int numRight(int[] answers, int[] student){
 		int sum=0;
@@ -28,22 +29,34 @@ public class Scanner {
 		return 0;
 	}
 	
-	public ArrayList<Integer> getStudentAnswers(PImage p) {
-		p.loadPixels();
+	public ArrayList<Integer> getAnswers(int[] pic) {
+		ArrayList<Integer> a = new ArrayList<Integer>;
 		for (int y = 223; y < 900; y = y + 49) {
+			int max = Integer.MIN_VALUE;
+			int count=0;
+			int maxcount=0;
 			for (int x = 222; x < 555; x = x + 71) {
-				if(isBlack(x,y))
+				count++;
+				if(Blackness(x,y,pic)>max){ 
+					maxcount =count; 
+					max = Blackness(x,y,pic);
+				}
 			}
+			a.add(maxcount);
 		}
 	}
 		
-	public boolean isBlack(int x,int y){
+	public int Blackness(int x,int y, int[] image){
+		int sum=0;
 		for(int i=x-10; i<x+10; i++ ){
 			for(int  j=y-10; j< y+10; j++){
-				if()
+				sum+=image[(i*scantron_length) + j];
 			}
 		}
+		return sum/400;
 	}
+	
+	
 	
 
 }
